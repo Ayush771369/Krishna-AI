fc019dfecf1f47da299d7da239d63feb6360da4b
Ayush771369 <ayusharch.77@gmail.com>
2026-05-23 14:11:38 +0530

    Reduce backend memory usage

--- backend/rag.py
+++ backend/rag.py
@@ -1,19 +1,10 @@
 import chromadb # type: ignore
-from chromadb.utils.embedding_functions import SentenceTransformerEmbeddingFunction # type: ignore
-
-# Embedding function
-embedding_function = SentenceTransformerEmbeddingFunction(
-    model_name="all-MiniLM-L6-v2"
-)
 
 # Load existing DB
 client = chromadb.PersistentClient(path="backend/db")
 
 # Load existing collection
-collection = client.get_collection(
-    name="gita",
-    embedding_function=embedding_function
-)
+collection = client.get_collection(name="gita")
 
 def retrieve_verses(query, n_results=3):
 
